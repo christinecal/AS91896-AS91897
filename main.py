129acf7b0c62bccf9ee649709b2659f9ce8a5058
Christine <st21361@ormiston.school.nz>
2022-06-13 14:01:10 +1200

    Set up error messages

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ Create all the default buttons, labels, and entry boxes. Put them in the correct
     #update(append),print,delete(row),quit
 
 def setup_buttons():
-    Button(main_window, text="Update",command=append_details) .grid(column=0,row=1)
+    Button(main_window, text="Update",command=check_inputs) .grid(column=0,row=1)
     Button(main_window, text="Print",command=print_hire_details) .grid(column=1,row=1)
     Button(main_window, text="Quit",command=quit) .grid(column=4, row=1)
     Button(main_window, text="Delete",command=delete_row) .grid(column=2,row=6)
 
-#create the labels (entries)
+#creating the labels (entries)
            #these are the global variables that are used, accessible throughout the program
     global hire_details, entry_name,entry_receiptnumber,entry_item,entry_numberhired, total_entries, delete_item
     #customer name
@@ -29,7 +29,7 @@ def setup_buttons():
     Label(main_window, text="Receipt Number") .grid(column=0,row=3)
     entry_receiptnumber = Entry(main_window)
     entry_receiptnumber.grid(column=1,row=3)
-    #item hired
+    #item hired (combobox)
     Label(main_window, text="Item Hired") .grid(column=0,row=4)
     item_hired = StringVar()
     entry_item = ttk.Combobox(main_window, textvariable=item_hired, values=('Cutlery Set', 'Glassware Set','Chairs', 'Tables', 'Pack of Balloons', 'Backdrop Hire',
@@ -50,7 +50,7 @@ def setup_buttons():
     #print the customers' details into a table
 
 def print_hire_details():
-    global total_entries, name_count, frame
+    global hire_details, total_entries, name_count, frame
     name_count = 0
     #column headings
     Label(frame, font='Helvetica 10 bold',text="Row").grid(column=0,row=7)
@@ -59,7 +59,7 @@ def print_hire_details():
     Label(frame, font='Helvetica 10 bold',text="Item Hired").grid(column=3,row=7)
     Label(frame, font='Helvetica 10 bold',text="Number Hired").grid(column=4,row=7)
     frame.grid(column=1,row=7)
-    #each row is the item on the list
+    #each item on the list as a separate row
     while name_count < total_entries :
         Label(frame, text=name_count).grid(column=0,row=name_count+8) 
         Label(frame, text=(hire_details[name_count][0])).grid(column=1,row=name_count+8)
@@ -68,26 +68,32 @@ def print_hire_details():
         Label(frame, text=(hire_details[name_count][3])).grid(column=4,row=name_count+8)
         name_count +=  1
 
-#UPDATE button
+#UPDATE (append) button
     #clear previous inputs to add the next customer to the list
         
 def append_details():
     global hire_details, entry_name,entry_receiptnumber,entry_item,entry_numberhired, total_entries
     #append each item to its own area of the list
-    hire_details.append([entry_name.get(),entry_receiptnumber.get(),entry_item.get(),entry_numberhired.get()])
+    hire_details.append([entry_name.get(),
+                         entry_receiptnumber.get(),
+                         entry_item.get(),
+                         entry_numberhired.get(),
+                         delete_item.get()])
     #clear the boxes
     entry_name.delete(0,'end')
     entry_receiptnumber.delete(0,'end')
     entry_item.delete(0,'end')
     entry_numberhired.delete(0,'end')
+    delete_item.delete(0, 'end')
     total_entries +=  1
     
 #DELETE button
     #delete a row when an item is returned
 def delete_row ():
     global hire_details, delete_item, total_entries, name_count
+    #which row to delete
     del hire_details[int(delete_item.get())]
-    total_entries -= 1
+    total_entries = - 1
     delete_item.delete(0,'end')
     for widget in frame.winfo_children():
         widget.destroy()
@@ -100,19 +106,51 @@ def quit():
     main_window.destroy()
 
 '''--------------- Check for validity ---------------
-Input cannot be blank and is specific to data type'''
- 
+Requirements:
+1. Input cannot be blank
+2. Specific to data type
+Set up error text messages'''
+
 def check_inputs():
-    pass
+    global hire_details,entry_name,entry_receiptnumber,entry_item,entry_numberhired,total_entries
+    check_input=0
+    Label(main_window, text="                                                                ") .grid(column=2, row=2)
+    Label(main_window, text="                                                                ") .grid(column=2, row=3)
+    Label(main_window, text="                                                                ") .grid(column=2, row=4)
+    Label(main_window, text="                                                                ") .grid(column=2, row=5)
+    #customer name (string)
+    if len(entry_name.get())==0 or type (entry_name)!= str:
+        Label(main_window,fg='red',text="Required (Letters)").grid(column=2,row=2, sticky=W)
+        check_input=1
+    #receipt number (integer)
+    if len(entry_receiptnumber.get())==0 or type (entry_receiptnumber)!= int:
+        Label(main_window,fg='red',text="Required (Numbers)").grid(column=2,row=3,sticky=W)
+        check_input=1
+    #item hired (fill the combobox)
+    if len(entry_item.get()) == 0:
+        Label(main_window,fg='red',text="Required (Select item)").grid(column=2,row=4,sticky=W)
+        check_input=1
+    #number hired (integer)
+    if (entry_numberhired.get().isdigit()):
+        if int(entry_numberhired.get()) < 1 or int(entry_numberhired.get()) > 500 or type (entry_numberhired)!= int:
+            Label(main_window,fg='red',text="Required (Numbers 1-500)").grid(column=2,row=5,sticky=W)
+            check_input=1
+    else:
+        Label(main_window,fg='red',text="Required (Numbers 1-500)").grid(column=2,row=5,sticky=W)
+        check_input=1
+    if check_input ==0:
+        append_details()
 
 '''----------- Start the program running -----------'''
 
 def main():
     global main_window 
     global hire_details, total_entries, frame
+    #empty list for the details
     hire_details = []
     total_entries = 0
     main_window = Tk()
+    #GUI 
     main_window.title("Julie's Party Hire")
     setup_buttons()
     frame = Frame(main_window)
